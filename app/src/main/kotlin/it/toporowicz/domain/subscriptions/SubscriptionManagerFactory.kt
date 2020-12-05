ea2a583126aac4c1840dec3727de5d503b351168
maciejtoporowicz <maciej.toporowicz@gmail.com>
2020-12-05 20:52:55 +0100

    register bean for subscription module

--- app/src/main/kotlin/it/toporowicz/domain/subscriptions/SubscriptionManagerFactory.kt
+++ app/src/main/kotlin/it/toporowicz/domain/subscriptions/SubscriptionManagerFactory.kt
@@ -3,12 +3,13 @@ package it.toporowicz.domain.subscriptions
 import com.google.cloud.firestore.Firestore
 import io.micronaut.context.annotation.Factory
 import it.toporowicz.domain.subscriptions.adapter.FirebaseBasedSubscriptionRepo
+import it.toporowicz.domain.subscriptions.core.SubscriptionModule
 import javax.inject.Singleton
 
 @Factory
 class SubscriptionManagerFactory {
     @Singleton
-    fun create(firestore: Firestore): FirebaseBasedSubscriptionRepo {
-        return FirebaseBasedSubscriptionRepo(firestore)
+    fun create(firestore: Firestore): SubscriptionModule {
+        return SubscriptionModule(FirebaseBasedSubscriptionRepo(firestore))
     }
 }
